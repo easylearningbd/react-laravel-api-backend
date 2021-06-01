fb4301dad982fe0641a902025256b288e8675405
easylearningbd <elebdsoft@gmail.com>
2021-06-02 02:45:04 +0600

    a6 Customize Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,83 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en" class="h-100">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>React - Admin Login Page </title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend/images/favicon.png') }} ">
+    <link href="{{ asset('backend/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }} " rel="stylesheet">
+    <link href="{{ asset('backend/css/style.css') }} " rel="stylesheet">
+  
+</head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <div class="text-center mb-3">
+                                        <img src="{{ asset('backend/images/logo_white.png') }}" alt="">
+                                    </div>
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                   
 
-        <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+        <div class="form-group">
+            <label class="mb-1"><strong>Email</strong></label>
+            <input type="email" id="email" name="email" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label class="mb-1"><strong>Password</strong></label>
+            <input type="password" id="password" name="password" class="form-control"  >
+        </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+            <div class="form-group">
+               <div class="custom-control custom-checkbox ml-1">
+                    <input type="checkbox" class="custom-control-input" id="basic_checkbox_1">
+                    <label class="custom-control-label" for="basic_checkbox_1">Remember my preference</label>
+                </div>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+            <div class="form-group">
+                <a href="page-forgot-password.html">Forgot Password?</a>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+        </div>
+    </form>
+                                    <div class="new-account mt-3">
+                                        <p>Don't have an account? <a class="text-primary" href="./page-register.html">Sign up</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('backend/vendor/global/global.min.js') }} "></script>
+    <script src="{{ asset('backend/vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }} "></script>
+    <script src="{{ asset('backend/js/custom.min.js') }} "></script>
+    <script src="{{ asset('backend/js/deznav-init.js') }} "></script>
+    
+    
+</body>
+</html>
